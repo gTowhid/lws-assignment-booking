134996f55e7e81227d1a549b060f5ae5ef2953f2
Towhid <towhidgolam8182@gmail.com>
2023-02-23 22:55:58 +0600

    table removed with zero entries

--- src/components/Previews.js
+++ src/components/Previews.js
@@ -8,16 +8,18 @@ export default function Previews() {
     <div>
       <div class="table-container">
         <table class="booking-table">
-          <thead class="bg-gray-100/50">
-            <tr class="text-black text-left">
-              <th>Destination From</th>
-              <th>Destination To</th>
-              <th class="text-center">Journey Date</th>
-              <th class="text-center">Guests</th>
-              <th class="text-center">Class</th>
-              <th class="text-center">Delete</th>
-            </tr>
-          </thead>
+          {state[0] && state[0].location && (
+            <thead class="bg-gray-100/50">
+              <tr class="text-black text-left">
+                <th>Destination From</th>
+                <th>Destination To</th>
+                <th class="text-center">Journey Date</th>
+                <th class="text-center">Guests</th>
+                <th class="text-center">Class</th>
+                <th class="text-center">Delete</th>
+              </tr>
+            </thead>
+          )}
           <tbody class="divide-y divide-gray-300/20" id="lws-previewBooked">
             {state.map(
               (el) => el.location && <Preview key={el.id} id={el.id} />
